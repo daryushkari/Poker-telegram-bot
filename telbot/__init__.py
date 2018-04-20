c8d74920ba2c8e5a44b03550f253501d176116d5
daryush kari <daryushkari123@gmail.com>
2018-04-20 22:25:57 +0430

    implement the logic of Court piece

---
+++ telbot/__init__.py
@@ -0,0 +1,11 @@
+from telbot.hokm.games import *
+from telbot.hokm.players import *
+
+
+def main():
+    lst = ["p1", "p2", "p3", "p4"]
+    StandardGame(lst, lst)
+
+
+if __name__ == "__main__":
+    main()
